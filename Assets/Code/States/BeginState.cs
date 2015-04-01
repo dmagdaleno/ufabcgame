4748950eecf70da2e710943d7a3af54e6d08f398
dmagdaleno <diegommagdaleno@gmail.com>
2015-04-01 00:14:57 -0300

    [Atualização do cenario do jogo]

--- Assets/Code/States/BeginState.cs
+++ Assets/Code/States/BeginState.cs
@@ -7,23 +7,22 @@ namespace Assets.Code.States {
 
 		public BeginState (StateManager manager) {
 			this.manager = manager;
-            		Debug.Log("Constructing BeginState");
+			Debug.Log("Constructing BeginState");
 		}
 
-	        public void Update() {
-	            
-	        }
+		public void Update() {
+
+		}
 
 		public void ShowIt () {
-            		if(GUI.Button(new Rect(10, 10, 150, 100), "Press to play")){
-            			Switch();
-            		}
+			if(GUI.Button(new Rect(10, 10, 150, 100), "Press to play")){
+				Switch();
+			}
 		}
-		
+
 		void Switch () {
-            		Application.LoadLevel("PlayScene01");
-	                manager.SwitchState(new PlayState(manager));
+			Application.LoadLevel("PlayScene01");
+			manager.SwitchState(new PlayState(manager));
 		}
-		
 	}
 }
